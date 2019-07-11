2aaf3b4b28e059250f2831f71330239d10dd5b08
isbaska <isbaska1@in.ibm.com>
2019-07-11 16:19:22 +0530

    Task-1step-1 and 3 is implemented

---
+++ src/main/java/com/stackroute/Main.java
@@ -0,0 +1,32 @@
+package com.stackroute;
+
+import com.stackroute.domain.Movie;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.*;
+import org.springframework.beans.factory.xml.ResourceEntityResolver;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+public class Main {
+		public static void main( String[] args )
+		{
+			ApplicationContext context = new FileSystemXmlApplicationContext("src/main/resources/beans.xml");
+			Movie movie1 = context.getBean("movieObj",Movie.class);
+			movie1.display();
+			
+			ApplicationContext context1 = new ClassPathXmlApplicationContext("beans.xml");
+			Movie movie2 = context1.getBean("movieObj",Movie.class);
+			movie2.display();
+			
+			BeanFactory factory = new XmlBeanFactory(new FileSystemResource("src/main/resources/beans.xml"));
+			Movie movie3 = factory.getBean("movieObj",Movie.class);
+			movie3.display();
+			
+		}
+
+}
---
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -0,0 +1,19 @@
+package com.stackroute.domain;
+
+public class Movie {
+	
+	private Actor actor;
+	
+	public Actor getActor() {
+		return actor;
+	}
+	
+	public void setActor(Actor actor) {
+		this.actor = actor;
+	}
+	
+	public void display()
+	{
+		System.out.println("Actor Name: "+actor.getName()+" Gender: "+actor.getGender()+"Age: "+actor.getAge() );
+	}
+}
