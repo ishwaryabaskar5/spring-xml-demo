d89c987eebf4e9f4595ccc079c4a6444c6d3e36d
isbaska <isbaska1@in.ibm.com>
2019-07-12 09:29:13 +0530

    Task 5 is implemented

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,48 +1,12 @@
 package com.stackroute;
 
-import com.stackroute.domain.Movie;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.BeanFactoryAware;
-import org.springframework.beans.factory.BeanNameAware;
+import com.stackroute.domain.BeanLifecycleDemoBean;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Main implements ApplicationContextAware, BeanFactoryAware, BeanNameAware {
-		public static void main( String[] args )
-		{
-			Main mainClass = new Main();
-//			Task 3
+public class Main {
+		public static void main( String[] args ) {
 			ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-			BeanFactory factory = new ClassPathXmlApplicationContext("beans.xml");
-			
-			mainClass.setApplicationContext(context);
-			mainClass.setBeanFactory(factory);
-			mainClass.setBeanName("movieObj");
+			BeanLifecycleDemoBean beanLifecycleDemoBean =  (BeanLifecycleDemoBean) context.getBean("beanLifeCycle");
 		}
-	
-	//		Task 4
-	@Override
-	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-		Movie movie = beanFactory.getBean("movieObj",Movie.class);
-		System.out.println("Inside setBeanFactory method");
-		movie.display();
-	}
-	
-	@Override
-	public void setBeanName(String s) {
-		BeanFactory factory = new ClassPathXmlApplicationContext("beans.xml");
-		Movie movie = factory.getBean(s,Movie.class);
-		System.out.println("Inside setBeanName method");
-		movie.display();
-	}
-	
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		Movie movie = applicationContext.getBean("movieObj",Movie.class);
-		System.out.println("Inside setApplicationContext method");
-		movie.display();
-	}
-
 }
