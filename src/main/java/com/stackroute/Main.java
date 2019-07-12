f44389cfd56c3d6914d20bcfab2437854686940c
isbaska <isbaska1@in.ibm.com>
2019-07-12 09:55:12 +0530

    Task 6 is implemented

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,12 +1,13 @@
 package com.stackroute;
 
 import com.stackroute.domain.BeanLifecycleDemoBean;
+import com.stackroute.domain.BeanPostProcessorDemoBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 		public static void main( String[] args ) {
 			ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-			BeanLifecycleDemoBean beanLifecycleDemoBean =  (BeanLifecycleDemoBean) context.getBean("beanLifeCycle");
+			BeanPostProcessorDemoBean beanPostProcessorDemoBean =  (BeanPostProcessorDemoBean) context.getBean("beanPostProcessorDemo");
 		}
 }
