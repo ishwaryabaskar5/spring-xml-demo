9f9140849f035ac3c2d3c72d9e4db462ab73156b
isbaska <isbaska1@in.ibm.com>
2019-07-12 11:30:24 +0530

    Task-6 is updated

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -2,12 +2,16 @@ package com.stackroute;
 
 import com.stackroute.domain.BeanLifecycleDemoBean;
 import com.stackroute.domain.BeanPostProcessorDemoBean;
+import com.stackroute.domain.Movie;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 		public static void main( String[] args ) {
 			ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+			
+			Movie movie = context.getBean("movieObj",Movie.class);
+			movie.display();
 			BeanPostProcessorDemoBean beanPostProcessorDemoBean =  (BeanPostProcessorDemoBean) context.getBean("beanPostProcessorDemo");
 		}
 }
